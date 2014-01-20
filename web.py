7a5814c103cddb0b85b1a2362d6a313cf03039f0
Emory Dunn <emory@emorydunn.com>
2014-01-19 21:46:45 -0800

    Fixes: - POST showing server info in browser.
    New Features:
    - The button toggles the switch.
    
    Upgrades:
    - Status page and state changes are entirely variable based, no more hard coded if/then.
    - Switch info is now in a table.

--- web.py
+++ web.py
@@ -1,3 +1,9 @@
+#Version: 0.5
+#Author: Emory Dunn
+
+# TODO POST showing server status in browser. With no content the self.send was sending the status. 
+# TODO If a switch goes offline have a reasonable timeout for loading the page. 
+
 #!/usr/bin/python
 from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
 from os import curdir, sep
@@ -16,10 +22,6 @@ def on_switch(switch):
     print "Switch found!", switch.name
     pass
 
-
-#status_html = "<head><title>Switch Status</title></head><h2>Switches from alias list:</h2>"
-
-
 #This class will handles any incoming request from
 #the browser 
 class myHandler(BaseHTTPRequestHandler):
@@ -27,14 +29,13 @@ class myHandler(BaseHTTPRequestHandler):
     #Handler for the GET requests
     def do_GET(self):
         #print(self.path)
+        # TODO Make any path not ending in an extension redirect to an HTML page. 
         if self.path=="/":
             self.path="/index.html"
         if self.path == '/status':
             self.path = '/status.html'
         if self.path == '/status/':
-            self.path = '/status.html'
-
-            
+            self.path = '/status.html'  
 
         try:
             #Check the file extension required and
@@ -61,26 +62,56 @@ class myHandler(BaseHTTPRequestHandler):
             if self.path.endswith(".css"):
                 mimetype='text/css'
                 sendReply = True
+            '''if self.path.endswith("/"):
+                print self.path
+                mimetype='text/html'
+                sendReply = True
+                self.path = self.path[:-1]+ ".html"
+                print self.path'''
                 
             parsed_path = urlparse.urlparse(self.path.replace('/', ''))
             #print parsed_path
+            #print 'Parsed_path: ' +parsed_path[2]
+            
+            #If there are params on the URL check what needs to be done
             try:
                 params = dict([p.split('=') for p in parsed_path[4].split('&')])
-                switches = True
-                sendReply = False
+                
+                #If params are on a URL that shouldn't accept them
+                if parsed_path[2] == (''):
+                    self.path = 'index.html'
+                    sendReply = True
+                elif parsed_path[2].endswith('.html'):
+                    self.path = parsed_path[2]
+                    sendReply = True
+                    #print 'Path: ' +self.path
+                else: 
+                    self.path = parsed_path[2]+ '.html'
+                    sendReply = True
+                    #print 'Path: ' +self.path
+                    
+                #Match the path of a POST request to redirect to the status page. 
+                if parsed_path[2] == 'status': 
+                    self.path = '/status.html'
+                    switches = False
+                    sendReply = True
+                    #print ('Status match')
+                elif parsed_path[2] == 'status.html':
+                    self.path = '/status.html'
+                    switches = False
+                    sendReply = True
+                    #print ('Status match')
             except:
                 params = {}
         
 
             if switches == True:
-                self.send_response(200)
-                self.send_header('Content-type',mimetype)
-                self.end_headers()
                 print ("State changes:")
                 for key in params:
                     self.name = wemo.get(key, None)
                     if self.name:
-                        #print self.name, params[key]
+                        print self.name, params[key]
+                        #If the paremter isn't on or off the state doesn't change. 
                         if params[key] == "on":
                             state = 1
                             #print state
@@ -88,72 +119,83 @@ class myHandler(BaseHTTPRequestHandler):
                             state = 0
                             #print state
                         try:
+                            #Set the state of the switch and print the result
                             switch = env.get_switch(self.name)
                             switch.basicevent.SetBinaryState(BinaryState=state)
-                            if switch.get_state() == 0:
-                                print ' -'+self.name,'is off.'
-                                self.stateH = 'off'
-                                self.wfile.write("<head><title>Switch Status</title></head>")
-                                #self.wfile.write("<head><title>"+self.name+": " +self.stateH+ "</title></head>")
-                                self.wfile.write(self.name+ " is currently " +self.stateH+ ". <br>")
-                            else:
-                        		#switch.get_state() == 1:
-                                print ' -'+self.name,'is on.'
-                                self.stateH = 'on'
-                                self.wfile.write("<head><title>Switch Status</title></head>")
-                                #self.wfile.write("<head><title>"+self.name+": " +self.stateH+ "</title></head>")
-                                self.wfile.write(self.name+ " is currently " +self.stateH+ ". <br> ")
+                            
+                            print ' -'+self.name+' is ' +params[key]+'.'
+                            
+#                           "" if switch.get_state() == 0:
+#                                print ' -'+self.name,'is off.'
+#                            else:
+#                        		#switch.get_state() == 1:
+#                                print ' -'+self.name,'is on.'""
                         except: 
                             print (' -'+self.name+ " doesn't exist.")
-                            self.wfile.write("<head><title>Switch Status</title></head>")
-                            #self.wfile.write("<head><title>"+self.name+": missing </title></head>")
-                            self.wfile.write(self.name+ " doesn't exist.<br>")
                             pass
             
             if sendReply == True:
-                '''print self.path
-                self.first, self.colon, self.rest = self.path.partition('?')
-                print self.first'''
                 #Open the static file requested and send it
                 self.send_response(200)
                 self.send_header('Content-type',mimetype)
                 self.end_headers()
-                try:
-                    self.wfile.write(self.name+ " is currently " +self.stateH+ ". ")
-                except: #The Status Board
-                    if self.path == '/status.html':
-                       
-                        status_html = open('status.html', 'r')
-                        content = status_html.read()
-                        print content
-                        self.wfile.write(content)
+                
+                #The Status Board
+                if self.path == '/status.html':
+                   
+                    status_html = open('status.html', 'r')
+                    page = status_html.read()
+                    #print content
+                    self.wfile.write(page)
+                    
+                    self.wfile.write("<div id=status>\n")
+                    self.wfile.write("<table>\n")
+                    
+                    print ("Switches from alias list:")
+                    
+                    for key in wemo:
+                        self.name = wemo.get(key, None)
                         
-                        print ("Switches from alias list:")
-                        '''self.wfile.write("<head><title>Switch Status</title></head>")
-                        self.wfile.write("<h2>Switches from alias list:</h2>")'''
+                        #print self.name
+                        try:
+                            switch = env.get_switch(self.name)
+                            #print switch
+                            print ("Checking state")
+                            if switch.get_state() == 0: #Set boolean states
+                                self.stateH = 'off'
+                                self.stateHi = 'on'
+                                #print ("State 0")
+                            elif switch.get_state() == 1:
+                                self.stateH = 'on'
+                                self.stateHi = 'off'
+                                #print ("State 0")
+                            else:
+                                print ("Bad State. ")
+                                pass
+                            
+                            print (' -'+self.name+' is '+self.stateH+'.')
+                            
+                            self.wfile.write("\n<tr>\n")
+                            self.wfile.write("<td>"+self.name+ " is currently " +self.stateH+ ".</td>")
+                            self.wfile.write("<td><a href='status?"+key+"="+self.stateHi+"'><img src='img/forward.png'></a></td>")
+                            self.wfile.write("\n</tr>\n")
+                        except:
+                            print (' -'+self.name+ " doesn't exist.")
+                            self.wfile.write("\n<tr>\n")
+                            self.wfile.write("<td>"+wemo[key]+ " doesn't exist.</td>")
+                            self.wfile.write("\n</tr>\n")
                         
-                        for key in wemo:
-                            self.name = wemo.get(key, None)
-                            #print self.name
-                            try:
-                                switch = env.get_switch(self.name)
-                                #print switch
-                                if switch.get_state() == 0:
-                                    print ' -'+self.name,'is off.'
-                                    self.stateH = 'off'
-                                    self.wfile.write("<div id=status>" +self.name+ " is currently " +self.stateH+ ". <img src='img/forward.png'></div> <br>")
-                                else:
-                            		#switch.get_state() == 1:
-                                    print ' -'+self.name,'is on.'
-                                    self.stateH = 'on'
-                                    self.wfile.write("<div id=status>" +self.name+ " is currently " +self.stateH+ ". <img src='img/forward.png'></div> <br>")
-                            except:
-                                print (' -'+self.name+ " doesn't exist.")
-                                self.wfile.write("<div id=status>" +wemo[key]+ " doesn't exist.<br></div>")
-                    else:
-                        f = open(curdir + sep + self.path) 
-                        self.wfile.write(f.read())
-                        f.close()
+                    self.wfile.write("</table>\n")
+                            
+                    self.wfile.write("\n</div>")
+                    
+                    self.wfile.write("\n\n</body>\n</html>")
+                            
+                else:
+                    f = open(curdir + sep + self.path)
+                    #print "Other: " +self.path
+                    self.wfile.write(f.read())
+                    f.close()
             
             return
 
